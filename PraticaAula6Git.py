fec6e2b5d05058b07760f92fdd4b7fcc3b546820
Brendow <brendow_sabe@hotmail.com>
2026-02-22 20:53:43 -0300

    importando bibliotecas Python

---
+++ PraticaAula6Git.py
@@ -0,0 +1,9 @@
+# 1. Crei um pasta local no seu computador e inicialie o git nessa pasta com git init (comando: 'git init' na pasta)
+# 2. adicione arquivos nessa pasta e veja as modificações com git status (feito novo python)
+# 3. utilize o git add e o git commit
+# 4. cre um novo repositorio no seu github
+# 5 conecte esse repositorio remoto com o seu repositorio local do seu computador e sincronize-OS 
+# 6 crie uma nova branch e navegue entre as branches
+# 7 crie um pull request no github e faça o merge
+
+
